60cd63dd1b4814e9489b8b4ce4b492b72381a092
Alexandr Ivanov <stuardryan14529@gmail.com>
2025-04-08 12:51:00 +0300

    Для Category список товаров сделан приватным, реализованы новые метод, геттер и сеттер, обновлены конфигурации тестов

--- src/cls_category.py
+++ src/cls_category.py
@@ -6,15 +6,36 @@ class Category:
 
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
     category_count = 0
     product_count = 0
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+    def __init__(self, name: str, description: str):
         """Инициализация экземпляров класса"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
         Category.category_count += 1
-        Category.product_count += len(products)
+
+    def add_product(self, product: Product) -> None:
+        """Метод для добавления товаров в категорию"""
+        for old_product in self.__products:
+            if old_product.name == product.name:
+                old_product.quantity += product.quantity
+                if old_product.price <= product.price:
+                    old_product.price = product.price
+                else:
+                    answer_input = input("Введите 'y' для подтверждения или 'n' для отмены действия").lower()
+                    if answer_input == "y":
+                        old_product.price = product.price
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Геттер, который выводит список товаров в виде строки"""
+        result = []
+        for product in self.__products:
+            result.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return "\n".join(result)
