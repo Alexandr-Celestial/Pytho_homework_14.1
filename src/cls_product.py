9d9cef32f420c31b5681d53d0c21c5eb5593dae4
Alexandr Ivanov <stuardryan14529@gmail.com>
2025-04-08 12:52:21 +0300

    Для Product атрибут цены  сделан приватным, реализованы новые метод, геттер и сеттер, обновлены конфигурации тестов

--- src/cls_product.py
+++ src/cls_product.py
@@ -1,3 +1,6 @@
+from typing import Self
+
+
 class Product:
     """Класс продукта"""
 
@@ -10,5 +13,22 @@ class Product:
         """Инициализация экземпляров класса"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, data_product: dict) -> Self:
+        """Класс-метод принимающий на вход параметры товара в словаре и возвращает созданный объект класса Product"""
+        return cls(**data_product)
+
+    @property
+    def price(self) -> float:
+        """Геттер для возврата значение приватного атрибута цены"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
--- tests/test_cls_product.py
+++ tests/test_cls_product.py
@@ -15,3 +15,17 @@ def test_init(fixture_product: Product) -> None:
     assert fixture_product.description == "256GB, Серый цвет, 200MP камера"
     assert fixture_product.price == 180000.0
     assert fixture_product.quantity == 1
+
+
+def test_price() -> None:
+    prod = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    prod.price = 125
+    assert prod.price == 125
+
+
+def test_new_product() -> None:
+    """Тест класс-метода new_product"""
+    test_data_dict = Product.new_product(
+        {"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}
+    )
+    assert isinstance(test_data_dict, Product)
