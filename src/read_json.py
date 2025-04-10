0a7c0b7405f234e8a7d6ce21a36cbdea3b06ca14
Alexandr Ivanov <stuardryan14529@gmail.com>
2025-04-10 16:14:33 +0300

    Обновлён README.md под новый функционалБ обновлён отчёт о покрытии тестамиБ обновлён main.py для вызова новых методов

--- src/read_json.py
+++ src/read_json.py
@@ -1,8 +1,10 @@
 import json
 
 
-def read_json_file():
+def read_json_file() -> list:
     """Функция для чтения json-файла"""
     with open("data/products.json", "r", encoding="utf-8") as file_path:
         data = json.load(file_path)
+        if not isinstance(data, list):
+            raise ValueError("JSON файл должен содержать список")
         return data
